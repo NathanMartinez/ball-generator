f84b6253a38c1d84248fe804d86063a88a6c40b4
Nate Martinez <natemartinez@Nates-iMac.local>
2021-01-30 19:29:51 -0700

    Added physics and other options

--- utilities/index.js
+++ utilities/index.js
@@ -1,11 +1,8 @@
-// Inputs
-const ballSizeInput = document.getElementById('ball-size')
-const randomSize = document.getElementById('random-size')
-const randomSizeMin = document.getElementById('random-size-min')
-const randomSizeMax = document.getElementById('random-size-max')
-const ballColorInput = document.getElementById('ball-color')
-const randomColor = document.getElementById('random-color')
-const ballCountInput = document.getElementById('ball-count')
+import Ball from '../Ball/index.js'
+
+// Canvas and Context
+const canvas = document.getElementById('canvas')
+const ctx = canvas.getContext('2d')
 
 const getRandomNumber = (min = 0, max = 9) => {
   min = Math.ceil(min);
@@ -15,7 +12,8 @@ const getRandomNumber = (min = 0, max = 9) => {
 const getRandomColor = () => {
   return `hsl(${getRandomNumber(0, 360)}, 100%, 50%)`
 }
-export const getRandomPosition = (radius) => {
+
+const getRandomPosition = (radius, ballsArray) => {
   // I want to add an option that will ensure the new position will
   // not collide with any existing ball.
   let x = getRandomNumber(radius, 500 - radius)
@@ -25,17 +23,68 @@ export const getRandomPosition = (radius) => {
     y
   }
 }
+
+export const addBalls = (ballsArray) => {
+  let {
+    ballCount
+  } = getValues()
+  for (let i = 0; i < ballCount; i++) {
+    let {
+      ballSize,
+      ballColor,
+      gravityValue
+    } = getValues()
+    let ball = new Ball(ctx, getRandomPosition(ballSize, ballsArray), ballSize, ballColor, gravityValue)
+    ballsArray.push(ball)
+  }
+  ballsArray.forEach(ball => {
+    ball.draw()
+  });
+}
+
+export const updateClear = () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
+}
+
+export const clearScreen = (ballsArray) => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ballsArray.splice(0, ballsArray.length)
+}
+// Inputs
+
+// Ball Size
+const ballSizeInput = document.getElementById('ball-size')
+const randomSize = document.getElementById('random-size')
+const randomSizeMin = document.getElementById('random-size-min')
+const randomSizeMax = document.getElementById('random-size-max')
+
+// Ball Color
+const ballColorInput = document.getElementById('ball-color')
+const randomColor = document.getElementById('random-color')
+
+// Ball Count
+const ballCountInput = document.getElementById('ball-count')
+
+// Gravity
+const gravity = document.getElementById('gravity')
+
+// Buttons
+export const AddBallButton = document.getElementById('add-ball-button')
+export const ClearScreenButton = document.getElementById('clear-screen-button')
+
 export const getValues = () => {
   let randomSizeMinVal = randomSizeMin.value ? randomSizeMin.value : 5
   let randomSizeMaxVal = randomSizeMax.value ? randomSizeMax.value : 50
   let randomSizeValue = randomSize.checked
   let ballSize = !randomSizeValue ? ballSizeInput.value : getRandomNumber(randomSizeMinVal, randomSizeMaxVal)
   let randomColorValue = randomColor.checked
+  let gravityValue = gravity.value
   let ballColor = !randomColorValue ? ballColorInput.value : getRandomColor()
   let ballCount = ballCountInput.value ? ballCountInput.value : 1
   return {
     ballSize,
     ballColor,
-    ballCount
+    ballCount,
+    gravityValue
   }
 }
